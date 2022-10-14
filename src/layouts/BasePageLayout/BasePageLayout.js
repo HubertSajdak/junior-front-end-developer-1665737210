e657898c8bcbed9b6850b33357902a7e9691d824
Hubert <hub.sajdak@gmail.com>
2022-10-14 12:39:30 +0200

    styled task list component

---
+++ src/layouts/BasePageLayout/BasePageLayout.js
@@ -0,0 +1,20 @@
+import styles from "./BasePageLayout.module.css";
+import logo from "../../images/logo.svg";
+import userPorfile from "../../images/userProfile.svg";
+const BasePageLayout = ({ children }) => {
+	return (
+		<div className={styles.baseLayout}>
+			<header className={styles.header}>
+				<div>
+					<img src={logo} alt="company logo" />
+				</div>
+				<div>
+					<img src={userPorfile} alt="user avatar" />
+				</div>
+			</header>
+			{children}
+		</div>
+	);
+};
+
+export default BasePageLayout;
